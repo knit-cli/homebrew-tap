ff4e36d54ca6cd3ad606ec6e7e017e2303c0c837
Ivaldi Desktop <ca2bc672-5b01-4186-a9fd-a0fa89e05727@sessions.t3.local>
2026-09-15 08:36:40 -0500

    Release Knit 0.1.0-alpha.15 to Homebrew
    Knit-Group: kg_20260915_449e09
    Knit-Bundle: release-knit-alpha-15-to-homebrew

--- Formula/knit.rb
+++ Formula/knit.rb
@@ -1,26 +1,26 @@
 class Knit < Formula
   desc "Local-first CLI for coordinating cross-repo feature bundles"
   homepage "https://github.com/knit-cli/knit"
-  version "0.1.0-alpha.14"
+  version "0.1.0-alpha.15"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/knit-cli/knit/releases/download/v#{version}/knit-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "c1602d5cb72bdb186882e4f6503711661079388e8c6cb09e8314e7702e8712da"
+      sha256 "374a42123f47742d327e6aebe4a287e7cc334a0695e505cfbb189d541ffd238c"
     else
       url "https://github.com/knit-cli/knit/releases/download/v#{version}/knit-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "9f00ab23dd540d4e0ba5b9596d7192380f0612f778c762c8a2dc68804249869b"
+      sha256 "48a5ef21f6b054eb2ed96fafb91fd8cae4412929989d85a613ec48f6b142a308"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/knit-cli/knit/releases/download/v#{version}/knit-v#{version}-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "f158507dcb6ef38669288177c5b1053d58593148da1ed9df701ccfbef024f4c3"
+      sha256 "3e4267c88d3ac885338864e92af789123a19d867c6d290dff32cffb1de4742d7"
     else
       url "https://github.com/knit-cli/knit/releases/download/v#{version}/knit-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "888dcff94b7c96ef28c19365ee80bcc4b68c2751c7ca345b009dddc37e3946c8"
+      sha256 "f506a9c1ab893ca871c277c393f8144ace0255e5f81d00db040c9329b92a0f80"
     end
   end
 
